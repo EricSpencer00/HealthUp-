3969dba3cebc462199ded18902ab2a18ae803ba9
ericspencer00 <espencer2@luc.edu>
2024-09-07 15:20:04 -0500

    remove watch folders

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,4 @@
 const { getDefaultConfig } = require('@react-native/metro-config');
-const path = require('path');
 
 /**
  * Metro configuration
@@ -13,14 +12,8 @@ const config = {
       /node_modules\/.*/
     ],
   },
-  watchFolders: [
-    // Add additional watch folders if needed
-    // Ensure the 'src' directory exists or adjust the path as necessary
-    path.resolve(__dirname, 'src'),
-  ],
 };
 
-// Extend the default config with custom settings
 module.exports = {
   ...getDefaultConfig(__dirname),
   ...config,
