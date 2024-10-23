1ab79f2bcfe643e6c394961dff13c65d041acfa5
cabmatthew <mcaballero3@luc.edu>
2024-10-23 14:46:49 -0500

    Added styling, pressable 'X' button

--- components/WorkoutDiary/WorkoutDiaryScreen.js
+++ components/WorkoutDiary/WorkoutDiaryScreen.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, StyleSheet, TextInput } from 'react-native';
+import { View, Text, Button, StyleSheet, TextInput, FlatList, TouchableOpacity, Pressable } from 'react-native';
 import { getFirestore, collection, addDoc, getDocs , query, where } from 'firebase/firestore'; 
 import { initializeApp } from 'firebase/app';
 
 import app from './../FirebaseConfig';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 const db = getFirestore(app);
 
 
@@ -36,10 +37,14 @@ export default function WorkoutDiary({ navigation }) {
       const newList = []
       // Arrow function in JS
       querySnapshot.forEach((doc) => {
-        newList.push(doc.data()["exercise"]);
-        console.log("Adding: ", doc.data());
+        newList.push({ 
+          id: doc.id, 
+          exercise: doc.data()["exercise"]
+        })
+          
+        console.log("Adding: ", doc.id, " => ", doc.data());
       });
-      setExerciseList(newList);
+      setExerciseList(newList); 
       console.log("Added exercises");
     } catch (error) {
       console.log("Error loading data: ", error);
@@ -61,33 +66,51 @@ export default function WorkoutDiary({ navigation }) {
     loadData();
   }, []);
 
+  // Function to access id of the clicked item
+  const handlePressedItem = (item) => {
+    console.log("Pressed: ", item.id)
+  }
+
+  // For FlatList. Renders each item with onPress, to trigger handlePressedItem on item
+  const renderItem = ({item})=>(
+    <View style={styles.listRow}>
+      <View style={styles.item}>
+        <Text>{item.exercise}</Text>
+      </View>
+      <TouchableOpacity style={styles.itemX} onPress={() => handlePressedItem(item)}>
+        <Text>X</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
         What exercises will you perform in today's workout?
       </Text>
 
-      {
-        exerciseList.map((exercise, index) => (
-          <Text key={index}>{exercise}</Text>
-        ))
-      }
-
-      <TextInput
-        value={exerciseName}
-        onChangeText={setExerciseName}
-        placeholder="Exercise Name"
-        style={styles.textBoxes}
+      <FlatList
+        style={styles.flatListStyle}
+        data={exerciseList}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
       />
+      
+      <View style={styles.inputContainer}>
+        <TextInput
+          value={exerciseName}
+          onChangeText={setExerciseName}
+          placeholder="Exercise Name"
+          style={styles.textBoxes}
+        />
+        <TouchableOpacity style={styles.submit} onPress={createAndLoad}>
+          <Text style={styles.submitText}>✔</Text>
+        </TouchableOpacity>
+      </View>
 
-      <Button title="Submit Data" onPress={createAndLoad} />
-
-      <Text>{'\n'}</Text>
-
-      <Button
-        title="START WORKOUT!"
-        onPress={() => navigation.navigate('CurrentWorkout')}
-      />
+      <TouchableOpacity style={styles.startWorkout} onPress={() => navigation.navigate('CurrentWorkout')}>
+        <Text style={styles.buttonText}>START WORKOUT!</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -96,21 +119,88 @@ export default function WorkoutDiary({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 20,
     backgroundColor: '#F5FCFF',
   },
   title: {
+    marginBottom: 10,
     fontSize: 24,
     fontWeight: 'bold',
   },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    marginBottom: 10,
+  },
   textBoxes: {
-    width: '90%',
+    flex: 1,
     fontSize: 18,
-    padding: 12,
+    padding: 7,
+    paddingHorizontal: 15,
     borderColor: 'gray',
-    borderWidth: 0.2,
+    borderWidth: 0.5,
     borderRadius: 10,
-    marginBottom: 20,
-  }
+  },
+  item: {
+    backgroundColor: '#ebf4ff',
+    borderRadius: 15,
+    fontSize: 17,
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    margin: 2,
+  },
+  itemX: {
+    backgroundColor: '#ffd7d4',
+    borderRadius: 15,
+    fontSize: 17,
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    margin: 2,
+  },
+  flatListStyle: {
+    flexGrow: 1,
+    margin: 5,
+    marginBottom: 10,
+  },
+  buttons: {
+    padding: 10,
+    borderRadius: 5,
+    margin: 10,
+    backgroundColor: '#4287f5',
+    width: '80%',
+    alignSelf: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  submit: {
+    backgroundColor: '#cff7b2',
+    color: 'white',
+    width: 45,
+    height: 45,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+  },
+  submitText: {
+    fontSize: 24,
+    color: 'black',
+  },
+  listRow: {
+    flexDirection: 'row',
+  },
+  startWorkout: {
+    padding: 10,
+    borderRadius: 5,
+    margin: 10,
+    width: '100%',
+    alignSelf: 'center',
+    backgroundColor: '#92d5f0',
+  },
 });
